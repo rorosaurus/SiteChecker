64637a163b2ba70a3e5d5bf471628e84102e8ac3
Dustin Specker <dustinspecker@dustinspecker.com>
2012-09-28 23:01:00 -0500

    Finds download link.

--- src/SiteChecker.java
+++ src/SiteChecker.java
@@ -19,9 +19,9 @@ public class SiteChecker {
 
     // TODO: prompt the user for these in a GUI
     // The URL to hit
-    //private static final String url = "http://www.awesomedl.com/search/label/How%20I%20Met%20Your%20Mother";
+    private static final String url = "http://www.awesomedl.com/search/label/How%20I%20Met%20Your%20Mother";
     // The string to search for at the URL (if that method is executed)
-    //private static final String phrase = "How I Met Your Mother Season 8";
+    private static final String phrase = "How I Met Your Mother Season 8";
     // The milliseconds in-between every refresh
     private static final int refreshDelay = 5000;
 
@@ -38,6 +38,7 @@ public class SiteChecker {
         final JTextField txtUrl = new JTextField();
         txtUrl.setLocation(80, 20);
         txtUrl.setSize(200, 30);
+        txtUrl.setText(url);
         frame.add(txtUrl);
 
         JLabel lblPhrase = new JLabel();
@@ -49,17 +50,28 @@ public class SiteChecker {
         final JTextField txtPhrase = new JTextField();
         txtPhrase.setLocation(80, 60);
         txtPhrase.setSize(200, 30);
+        txtPhrase.setText(phrase);
         frame.add(txtPhrase);
 
-        final JButton btnCheck = new JButton();
+        JButton btnCheck = new JButton();
         btnCheck.setLocation(100, 100);
         btnCheck.setSize(100, 30);
         btnCheck.setText("Check");
         btnCheck.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                btnCheck.setText("Scanning...");
-                scanForChange(txtUrl.getText(), txtPhrase.getText());
-                btnCheck.setText("Check");
+                //scanForChange(txtUrl.getText());
+                String episodePage = getEpisodePage(txtUrl.getText(), txtPhrase.getText());
+                String downloadPage = null;
+                String actualDownloadPage = null;
+                if(episodePage != null) {
+                    downloadPage = getLinkFromRight(episodePage, "\">Mediafire", "href=\"");
+                }
+                if(downloadPage != null) {
+                    actualDownloadPage = getLinkFromLeft(downloadPage, "var url = '", "';");
+                }
+                if(actualDownloadPage != null) {
+                    alertUser(actualDownloadPage);
+                }
             }
         });
         frame.add(btnCheck);
@@ -72,43 +84,94 @@ public class SiteChecker {
         frame.setVisible(true);
     }
 
-    /*public static void scanForString(){
+    //gets link to episode download page
+    public static String getEpisodePage(String url, String phrase) {
+        String oldPage = getWebPage(url);
+
+        //while(true){
+            String webpage = getWebPage(url);
+            //if(webpage != oldPage) {
+                if(webpage.contains(phrase)){
+                    String[] content = webpage.split("'>" + phrase)[0].split("href='");
+                    return content[content.length-1];
+                }
+                // Otherwise, try again in five seconds
+                else{
+                    try {
+                        Thread.sleep(refreshDelay);
+                    } catch (InterruptedException e) {
+                        handleException(e);
+                    }
+                }
+            //}
+        //}
+        return null;
+    }
+
+    //gets desired url
+    //string1 = text to the right of link
+    //string2 = text to the left of link
+    public static String getLinkFromRight(String url, String string1, String string2){
+        String webpage = getWebPage(url);
+        if(webpage.contains(string1)){
+            String[] content = webpage.split(string1)[0].split(string2);
+            return content[content.length-1];
+        }
+        return null;
+    }
+
+    //gets desired url
+    //string1 = text to the right of link
+    //string2 = text to the left of link
+    public static String getLinkFromLeft(String url, String string1, String string2){
+        String webpage = getWebPage(url);
+        if(webpage.contains(string1)){
+            String[] content = webpage.split(string1)[1].split(string2);
+            return content[0];
+        }
+        return null;
+    }
+
+    //get mediafire download page
+    public static String getDownloadPage(String downloadPage){
+        // Initialize variables
+        String webpage = getWebPage(downloadPage);
+
+        if(webpage.contains("Mediafire")){
+            String[] content = webpage.split("\">Mediafire")[0].split("href=\"");
+            alertUser(content[content.length-1]);
+            return content[content.length-1];
+        }
+        return null;
+    }
+
+    public static String getWebPage(String url){
         // Initialize variables
         String webpage = "";
         String inputLine;
 
         // TODO: split this stuff into some a little more OOP
         try {
-            while(true){
-                // Re-connect
-                URL site = new URL(url);
-                URLConnection connection = site.openConnection();
-                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                // Reset newHTML to read in new contents
-                webpage = "";
+            URL site = new URL(url);
+            URLConnection connection = site.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            // Reset newHTML to read in new contents
+            webpage = "";
 
-                // Read in new contents
-                while ((inputLine = in.readLine()) != null) {
-                    webpage += inputLine;
-                }
-                in.close();
-
-                // Compare new and old.  If there's a change, notify the user and break.
-                if(webpage.contains(phrase)){
-                    alertUser("The string \"" + phrase + "\" has been found at the URL \"" + url + "\"!");
-                    break;
-                }
-                // Otherwise, try again in five seconds
-                else{
-                    Thread.sleep(refreshDelay);
-                }
+            // Read in new contents
+            while ((inputLine = in.readLine()) != null) {
+                webpage += inputLine;
             }
+            in.close();
+            return webpage;
         } catch (Exception e) {
             handleException(e);
         }
-    } */
 
-    public static void scanForChange(String url, String phrase){
+        return null;
+    }
+
+    public static void scanForChange(String url){
         // TODO: check for null strings and valid url
 
         // Initialize variables
